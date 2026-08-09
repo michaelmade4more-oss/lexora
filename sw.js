07eb5cf637a8136585654f840392de79fb764f2a
michaelmade4more-oss <michaelmade4more@gmail.com>
2026-08-09 19:29:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lexora-v1";
+const CACHE = "lexora-v2";
 const SHELL = ["./", "./index.html", "./app.jsx", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
